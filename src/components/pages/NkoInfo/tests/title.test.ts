6b71fc0ab9078703445facf902bbed4325a5947d
Lustach <lusta.vlad2001@gmail.com>
2022-05-26 17:28:38 +0300

    Add: formcategoryTitile Nko

--- src/components/pages/NkoInfo/tests/title.test.ts
+++ src/components/pages/NkoInfo/tests/title.test.ts
@@ -1,26 +1,32 @@
 import ChFormCategoryTitle from "@/components/pages/NkoInfo/ChFormCategoryTitle.vue";
 
 import { mount } from '@vue/test-utils'
-import { describe, expect, it, beforeEach } from 'vitest'
+import { describe, expect, it} from 'vitest'
 describe('ChFormCategoryTitle', () => {
-    // const wrapper = mount(ChFormCategoryTitle)
-    // beforeEach(() => {
-    //     const props = {
-    //         title: "Ваши контактные данные",
-    //         line: false
-    //     }
-    //     wrapper.setProps(props)
-    // })
-    // it('Картинка не отображается при line=false', () => {
-    //     expect(wrapper.find('.line').exists()).toBeFalsy();
-    // })
     it('Картинка отображается при line=true', () => {
         const props = {
             line: true
         }
+        const wrapper = mount(ChFormCategoryTitle, { props: props })
+        expect(wrapper.find('.line').exists());
+    })
+    it('Картинка не отображается при line=false', () => {
+        const props = {
+            line: false
+        }
+        const wrapper = mount(ChFormCategoryTitle, { props: props })
+        expect(wrapper.find('.line').exists()).toBe(false)
+    })
+    it('h2 отображается всегда', () => {
         const wrapper = mount(ChFormCategoryTitle)
-        // wrapper.setProps(props)
-        console.log(wrapper.find('.line').exists());
+        expect(wrapper.find('h2').exists()).toBe(true)
+    })
+    it('Текст отображается корректно', () => {
+        const props = {
+            title: "Ваши контактные данные",
+        }
+        const wrapper = mount(ChFormCategoryTitle, { props: props })
+        expect(wrapper.text()).toBe('Ваши контактные данные')
     })
 }
 )
